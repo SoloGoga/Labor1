cc3dde68e367259643bce01e9f298d330beb8031
SoloGoga <48013653+SoloGoga@users.noreply.github.com>
2019-03-10 20:18:42 +0200

    Update Program.cs

--- labor1/Program.cs
+++ labor1/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labor1
+namespace lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double mnoz1, mnoz2, ans;
+
+            Console.WriteLine("Введите числа, произведения которых вы хотите найти.");
+
+            mnoz1 = Convert.ToDouble(Console.ReadLine());
+
+            mnoz2 = Convert.ToDouble(Console.ReadLine());
+
+            ans = mnoz1 * mnoz2;
+
+            Console.Write(mnoz1 + " * " + mnoz2 + " = " + ans);
+
+            Console.ReadLine();
         }
+
     }
 }
